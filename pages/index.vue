67cb3270e2df95063c5b56b3700fff9f5db19b02
keita-naito <atiek_nn_xx@yahoo.co.jp>
2021-01-22 12:58:34 +0900

    記事一覧を表示するカードを用意し、最低限の見た目を調整

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>{{ articles }}</div>
+  <v-container :class="$style.container">
+    <v-card tile flat :class="$style.card">
+      <div v-for="article in articles" :key="article.id">
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title :class="$style.article_title">
+              <nuxt-link to="/">{{ article.title }}</nuxt-link>
+            </v-list-item-title>
+            <v-list-item-subtitle :class="$style.user_name">
+              by {{ article.user.name }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider class="mx-4"></v-divider>
+      </div>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
@@ -15,3 +31,31 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" module>
+.container {
+  margin-top: 20px;
+}
+.card {
+  margin: 0 auto;
+  padding: 28px 20px;
+  width: 800px;
+}
+.article_title {
+  a {
+    color: #000;
+    font-weight: bold;
+    text-decoration: none;
+    font-size: 20px;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  a:visited {
+    color: #777;
+  }
+}
+.user_name {
+  font-size: 16px;
+}
+</style>
